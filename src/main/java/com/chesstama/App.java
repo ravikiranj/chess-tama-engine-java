0fa417f11c81f4d4202697b5fe36f6e42e939d95
Ravikiran Janardhana <ravijan@amazon.com>
2018-12-20 15:23:25 -0800

    Added mobility score

--- src/main/java/com/chesstama/App.java
+++ src/main/java/com/chesstama/App.java
@@ -2,12 +2,14 @@ package com.chesstama;
 
 import com.chesstama.engine.Board;
 import com.chesstama.engine.Evaluator;
+import com.chesstama.engine.Player;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class App {
     public static void main(final String[] args) {
         Board board = new Board.Builder()
+                               .withCurrentPlayer(Player.P1)
                                .build();
         log.info("Board = {}", board);
         board.printBoardState();
--- src/main/java/com/chesstama/engine/Evaluator.java
+++ src/main/java/com/chesstama/engine/Evaluator.java
@@ -1,5 +1,8 @@
 package com.chesstama.engine;
 
+import com.google.common.collect.ImmutableList;
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
@@ -7,10 +10,16 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @SuppressWarnings({"Duplicates"})
+@Slf4j
 public final class Evaluator {
-    private static final int OPP_KING_CAPTURE = Integer.MAX_VALUE;
-    private static final int OPP_KING_HOME_MOVE = Integer.MAX_VALUE;
-    private static final int OPP_PAWN_CAPTURE = Byte.MAX_VALUE;
+    private static final int BASE_SCORE = 10;
+
+    private static final int OPP_KING_CAPTURE = BASE_SCORE * 1000;
+    private static final int OPP_KING_HOME_MOVE = BASE_SCORE * 1000;
+
+    private static final int OPP_PAWN_CAPTURE = BASE_SCORE * 100;
+
+    private static final int UNIQUE_MOVE = BASE_SCORE * 25;
 
     private static final Position P1_KING_SLOT = new Position(4, 2);
     private static final Position P2_KING_SLOT = new Position(0, 2);
@@ -24,10 +33,48 @@ public final class Evaluator {
 
         value += getKingScore(board, validMoves);
         value += getPawnsScore(board, validMoves);
+        value += getMobilityScore(board, validMoves);
 
         return value;
     }
 
+    /**
+     * @return score reflecting unique reachable positions on the board
+     */
+    private static long getMobilityScore(final Board board, final List<Position> validMoves) {
+        Player currentPlayer = board.getCurrentPlayer();
+
+        List<Position> piecePositions = new ImmutableList.Builder<Position>()
+            .add(board.getKingPosition(currentPlayer))
+            .addAll(board.getPawnPositions(currentPlayer))
+            .build();
+
+        Set<Position> uniqueValidMoves = new HashSet<>();
+
+        for (Position piecePosition : piecePositions) {
+            for (Position validMove : validMoves) {
+                Position potentialMovePos = piecePosition.add(validMove);
+                if (potentialMovePos.isValid() && !uniqueValidMoves.contains(potentialMovePos)) {
+                    uniqueValidMoves.add(potentialMovePos);
+                }
+            }
+        }
+
+        long score = uniqueValidMoves.size() * UNIQUE_MOVE;
+
+        log.info("Unique Valid Moves = {}, Count = {}, Score = {}",
+            uniqueValidMoves, uniqueValidMoves.size(), score);
+
+        return score;
+    }
+
+    /**
+     * 1) Opponent King Capture
+     * 2) Opponent King Home Move
+     * 3) Opponent Pawn Capture
+     *
+     * @return king's score reflecting above criteria
+     */
     private static long getKingScore(final Board board,
                                      final List<Position> validMoves) {
         long score = 0L;
@@ -62,6 +109,12 @@ public final class Evaluator {
         return score;
     }
 
+    /**
+     * 1) Opponent King Capture
+     * 2) Opponent Pawn Capture
+     *
+     * @return pawns' score reflecting above criteria
+     */
     private static long getPawnsScore(final Board board,
                                       final List<Position> validMoves) {
         long score = 0L;
